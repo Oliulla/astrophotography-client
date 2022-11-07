d7a2982442c501e92afac90fd8909274f19b9663
Oliulla <razasm29@gmail.com>
2022-11-08 00:57:23 +0600

    set up router

--- src/App.js
+++ src/App.js
@@ -1,45 +1,14 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
-import { Navbar } from 'flowbite-react';
-import brandImg from './assets/img/brandImg.jpg'
+import {router} from './Routes/Router';
+
+
 
 function App() {
   return (
-    <Navbar
-    className='dark-theme'
-    fluid={true}
-  >
-    <Navbar.Brand>
-      <img
-        src={brandImg}
-        className="mr-3 h-6 sm:h-9 border-blue-700 rounded-full"
-        alt="Flowbite Logo"
-      />
-      <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-        Astrography
-      </span>
-    </Navbar.Brand>
-    <Navbar.Toggle />
-    <Navbar.Collapse>
-      <Navbar.Link
-        href="/navbars"
-        active={true}
-      >
-        Home
-      </Navbar.Link>
-      <Navbar.Link href="/navbars">
-        About
-      </Navbar.Link>
-      <Navbar.Link href="/navbars">
-        Services
-      </Navbar.Link>
-      <Navbar.Link href="/navbars">
-        Pricing
-      </Navbar.Link>
-      <Navbar.Link href="/navbars">
-        Contact
-      </Navbar.Link>
-    </Navbar.Collapse>
-  </Navbar>
+    <div>
+        <RouterProvider router={router} />
+    </div>
   );
 }
 
---
+++ src/Routes/Router.js
@@ -0,0 +1,19 @@
+import { createBrowserRouter} from 'react-router-dom';
+import ErrorPage from '../pages/Shared/ErrorPage/ErrorPage';
+import Home from '../pages/Shared/Home/Home';
+import Root from '../Root/Root';
+
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            }
+        ]
+    }
+])
