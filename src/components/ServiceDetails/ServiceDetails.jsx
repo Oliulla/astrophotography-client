8f33ec3ef7b7f76db297888f924a21f7478679c7
Oliulla <razasm29@gmail.com>
2022-11-09 21:20:44 +0600

    add review form and post data to mongodb

--- src/components/ServiceDetails/ServiceDetails.jsx
+++ src/components/ServiceDetails/ServiceDetails.jsx
@@ -1,37 +1,77 @@
 import { StarIcon } from "@heroicons/react/24/solid";
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+import AddReviewForm from "../AddReviewForm/AddReviewForm";
 
 const ServiceDetails = () => {
   const { data } = useLoaderData({});
-  const { servImg, servName, servDesc, servPrice, servRating } = data;
+  const { servImg, servName, servDesc, servPrice, servRating, _id } = data;
+  const { user } = useContext(AuthContext);
 
   return (
-    <div className="flex flex-col items-center mt-10 mb-24">
-      <section className="card card-compact w-10/12 md:w-6/12 bg-black shadow-xl">
-        <figure>
-          <img src={servImg} alt={servName} className="w-full h-72" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{servName}</h2>
-          <p>{servDesc}</p>
-          <div className="grid grid-cols-2 justify-evenly my-8">
-            <p className="text-xl text-red-700">Price: {servPrice}</p>
-            <div>
-            <p className="text-xl text-yellow-500"> {servRating}
-            <StarIcon className="w-4 h-4 inline" />
-            <StarIcon className="w-4 h-4 inline" />
-            <StarIcon className="w-4 h-4 inline" />
-            <StarIcon className="w-4 h-4 inline" />
-            <StarIcon className="w-4 h-4 inline" />
-            </p>
+    <div className="flex flex-col mt-10 mb-24 md:mx-16">
+      <div className="grid md:grid-cols-2 justify-center ">
+        <div className="card card-compact w-full bg-black shadow-xl">
+          <figure>
+            <img src={servImg} alt={servName} className="w-full h-72" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title text-white text-3xl">{servName}</h2>
+            <p className="text-[1rem]">{servDesc}</p>
+            <div className="grid grid-cols-2 justify-evenly my-8">
+              <p className="text-xl text-red-700">Price: {servPrice}</p>
+              <div>
+                <p className="text-xl text-yellow-500">
+                  {" "}
+                  {servRating}
+                  <StarIcon className="w-4 h-4 inline" />
+                  <StarIcon className="w-4 h-4 inline" />
+                  <StarIcon className="w-4 h-4 inline" />
+                  <StarIcon className="w-4 h-4 inline" />
+                  <StarIcon className="w-4 h-4 inline" />
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </section>
-      <div className="h-[2px] w-full bg-blue-600 my-10"></div> 
+        <div className="mt-4 md:mt-0">
+          {user?.uid ? (
+            <AddReviewForm serviceId={_id} />
+          ) : (
+            <div>
+              <h2 className="text-center text-4xl text-white mt-20">
+                Plaease{" "}
+                <em>
+                  <Link to="/login" className="text-info font-bold underline">
+                    Login
+                  </Link>
+                </em>{" "}
+                First To Add Review
+              </h2>
+            </div>
+          )}
+          <div>
+            <h2 className="text-center text-4xl text-white mt-20">
+              Please! <br />
+              Scroll Down To Read Others Review
+            </h2>
+          </div>
+        </div>
+      </div>
+      <div className="h-[2px] w-full bg-blue-600 my-10"></div>
       <section className="mb-20 text-4xl">
         <h2>User Reviews section</h2>
+        <div className="card w-96 bg-black shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -51,9 +51,9 @@ const Home = () => {
       <section>
         <div className="text-center bg-black py-10">
         <h2 className="text-5xl font-semibold text-info">Get In Touch</h2>
-        <div className="my-6">
-          <input className="px-2 py-3 w-60 border border-blue-500 text-white" type="email" placeholder="email" /> 
-          <button className="bg-blue-600 py-3 px-4 text-xl font-semibold text-white">Subscribe</button>
+        <div className="my-6 flex justify-center">
+          <input className="px-2 py-3 w-60 border outline-none border-blue-500 text-white" type="email" placeholder="email" /> 
+          <button className="bg-blue-600 hover:bg-blue-800 py-3 px-4 text-xl font-semibold text-white">Subscribe</button>
         </div>
         </div>
       </section>
